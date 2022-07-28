836b293add531706e88dc190cd3cf9b1701c39e9
Bicky Tamang <bickytamang101@gmail.com>
2022-07-28 13:44:35 +0545

    Added sidebar, discover and video view

---
+++ components/NoResults.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface IProps {
+  text: string;
+}
+
+function NoResults({ text }: IProps) {
+  return <div>NoResults</div>;
+}
+
+export default NoResults;
---
+++ pages/index.tsx
@@ -0,0 +1,32 @@
+import type { NextPage } from "next";
+import axios from "axios";
+import { Video } from "../types";
+import NoResults from "../components/NoResults";
+import VideoCard from "../components/VideoCard";
+
+interface IProps {
+  videos: Video[];
+}
+
+const Home = ({ videos }: IProps) => {
+  return (
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+      ) : (
+        <NoResults text={"No Videos"} />
+      )}
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: {
+      videos: data,
+    },
+  };
+};
+export default Home;
